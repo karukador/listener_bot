933d40554eeb2f65a14340bf30952b5fdd9c7c39
karukador <152086077+karukador@users.noreply.github.com>
2024-04-26 10:19:38 +0300

    Update system_config.py
    опять что-то с кодировкой

--- system_config.py
+++ system_config.py
@@ -1,9 +1,9 @@
 URL = "https://stt.api.cloud.yandex.net/speech/v1/stt:recognize?"
-LANGUAGE = "ru-RU" # распознаём голосовое сообщение на русском языке
-model_version = "general" # используем основную версию модели
+LANGUAGE = "ru-RU"  # СЂР°СЃРїРѕР·РЅР°С‘Рј РіРѕР»РѕСЃРѕРІРѕРµ СЃРѕРѕР±С‰РµРЅРёРµ РЅР° СЂСѓСЃСЃРєРѕРј СЏР·С‹РєРµ
+model_version = "general"  # РёСЃРїРѕР»СЊР·СѓРµРј РѕСЃРЅРѕРІРЅСѓСЋ РІРµСЂСЃРёСЋ РјРѕРґРµР»Рё
 
 CONTENT_TYPES = ["text", "audio", "document", "photo", "sticker", "video", "video_note", "voice"]
-ADMIN_ID: int = 1234 # сюда введите ваш telegram_id
-MAX_USER_STT_BLOCKS = 12  # выделяем на каждого пользователя по 12 аудиоблоков
+ADMIN_ID: int = 1234  # СЃСЋРґР° РІРІРµРґРёС‚Рµ РІР°С€ telegram_id
+MAX_USER_STT_BLOCKS = 12  # РІС‹РґРµР»СЏРµРј РЅР° РєР°Р¶РґРѕРіРѕ РїРѕР»СЊР·РѕРІР°С‚РµР»СЏ РїРѕ 12 Р°СѓРґРёРѕР±Р»РѕРєРѕРІ
 
 DB_NAME = "speech_kit.db"
